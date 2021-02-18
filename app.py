a19efe8477cc7a51a1bfe472d04a83013814e83f
user01 <cukid9876@outlook.com>
2021-02-17 22:08:42 -0800

    query database for venues page

--- app.py
+++ app.py
@@ -90,30 +90,23 @@ def index():
 
 @app.route('/venues')
 def venues():
-  # TODO: replace with real venues data.
-  #       num_shows should be aggregated based on number of upcoming shows per venue.
-  data=[{
-    "city": "San Francisco",
-    "state": "CA",
-    "venues": [{
-      "id": 1,
-      "name": "The Musical Hop",
-      "num_upcoming_shows": 0,
-    }, {
-      "id": 3,
-      "name": "Park Square Live Music & Coffee",
-      "num_upcoming_shows": 1,
-    }]
-  }, {
-    "city": "New York",
-    "state": "NY",
-    "venues": [{
-      "id": 2,
-      "name": "The Dueling Pianos Bar",
-      "num_upcoming_shows": 0,
-    }]
-  }]
-  return render_template('pages/venues.html', areas=data);
+  data = []
+  venues = Venue.query.all()
+  places = Venue.query.distinct(Venue.city, Venue.state).all()
+  for place in places:
+    data.append({
+      'city': place.city,
+      'state': place.state,
+      'venues': [{
+        'id': venue.id,
+        'name': venue.name,
+      }
+    for venue in venues if
+    venue.city == place.city and venue.state == place.state]
+    })
+
+  print(data)
+  return render_template('pages/venues.html',areas=data)
 
 @app.route('/venues/search', methods=['POST'])
 def search_venues():
