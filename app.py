d1d6e383cea764ab6a3e9a4e75310093b5a4caf3
user01 <cukid9876@outlook.com>
2021-03-01 22:15:37 -0800

    color delete venue button red flash success message and return to homepage after venue delete

--- app.py
+++ app.py
@@ -182,7 +182,8 @@ def delete_venue(venue_id):
 
   # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
   # clicking that button delete it from the db then redirect the user to the homepage
-  return None
+  flash('Venue was successfully deleted!')
+  return render_template('pages/home.html')
 
 #  Artists
 #  ----------------------------------------------------------------
