7fb3488479dfb91bdd4ddc3f3810276ed22f9c9b
nickiapalucci <cukid9876@outlook.com>
2021-07-24 00:02:46 +0000

    remove completed todos fix model declaration indentation

--- app.py
+++ app.py
@@ -28,44 +28,42 @@ csrf.init_app(app)
 
 migrate = Migrate(app, db)
 
-# TODO: connect to a local postgresql database
-
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#
 
 class Venue(db.Model):
-    __tablename__ = 'Venue'
+  __tablename__ = 'Venue'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean())
-    seeking_description = db.Column(db.String())
-    genres = db.Column(db.ARRAY(db.String()))
-    website = db.Column(db.String(120))
-    shows = db.relationship('Show', backref='Venue', passive_deletes=True, lazy=True)
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String)
+  city = db.Column(db.String(120))
+  state = db.Column(db.String(120))
+  address = db.Column(db.String(120))
+  phone = db.Column(db.String(120))
+  image_link = db.Column(db.String(500))
+  facebook_link = db.Column(db.String(120))
+  seeking_talent = db.Column(db.Boolean())
+  seeking_description = db.Column(db.String())
+  genres = db.Column(db.ARRAY(db.String()))
+  website = db.Column(db.String(120))
+  shows = db.relationship('Show', backref='Venue', passive_deletes=True, lazy=True)
 
 class Artist(db.Model):
-    __tablename__ = 'Artist'
+  __tablename__ = 'Artist'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String()))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean())
-    seeking_description = db.Column(db.String())
-    website = db.Column(db.String(120))
-    shows = db.relationship('Show', backref='Artist', passive_deletes=True, lazy=True)
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String)
+  city = db.Column(db.String(120))
+  state = db.Column(db.String(120))
+  phone = db.Column(db.String(120))
+  genres = db.Column(db.ARRAY(db.String()))
+  image_link = db.Column(db.String(500))
+  facebook_link = db.Column(db.String(120))
+  seeking_venue = db.Column(db.Boolean())
+  seeking_description = db.Column(db.String())
+  website = db.Column(db.String(120))
+  shows = db.relationship('Show', backref='Artist', passive_deletes=True, lazy=True)
 
 class Show(db.Model):
   __tablename__ = 'Show'
