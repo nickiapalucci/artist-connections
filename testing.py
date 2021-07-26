7229d4b8e4ae133204d6f19464bc0a3776731b4a
nickiapalucci <cukid9876@outlook.com>
2021-07-26 15:15:43 +0000

    import all from app.py in testing.py use random.shuffle as function new db objects declared in each for loop picsum random url uses seed new show uses artist_id and venue_id from queried list

--- testing.py
+++ testing.py
@@ -1,4 +1,4 @@
-from . import app
+from app import *
 from app import Venue, Artist, Show
 import random, string
 from flask_sqlalchemy import SQLAlchemy
@@ -36,17 +36,14 @@ def genres_filler():
             "Other",
         ]
     
-    unique_genres_list = random.shuffle(unique_genres_list)
+    random.shuffle(unique_genres_list)
     unique_genres_list = unique_genres_list[:random.randrange(1,4)]
 
     return unique_genres_list
 
 def fill_database(quantity=10):
-    new_venue = app.Venue()
-    new_artist = app.Artist()
-    newshow = app.Show()
-    
     for x in range(quantity):
+        new_venue = Venue()
         new_venue.name = random_string()
         new_venue.genres = genres_filler()
         new_venue.address = random_string()
@@ -57,11 +54,12 @@ def fill_database(quantity=10):
         new_venue.facebook_link = "https://www.facebook.com/Udacity/"
         new_venue.seeking_talent = bool(random.getrandbits(1))
         new_venue.seeking_description = random_string(20)
-        new_venue.image_link = "https://picsum.photos/seed/" + random_string() + "/picsum/200/300"
+        new_venue.image_link = "https://picsum.photos/seed/" + random_string() + "/200/300"
         db.session.add(new_venue)
         db.session.commit()
     
     for x in range(quantity):
+        new_artist = Artist()
         new_artist.name = random_string()
         new_artist.genres = genres_filler()
         new_artist.city = random_string()
@@ -71,13 +69,18 @@ def fill_database(quantity=10):
         new_artist.facebook_link = "https://www.facebook.com/Udacity/"
         new_artist.seeking_venue = bool(random.getrandbits(1))
         new_artist.seeking_description = random_string(20)
-        new_artist.image_link = "https://picsum.photos/seed/" + random_string() + "/picsum/200/300"
+        new_artist.image_link = "https://picsum.photos/seed/" + random_string() + "/200/300"
         db.session.add(new_artist)
         db.session.commit()
     
-    for x in range(quantity / 4):
-        newshow.venue_id = random.randrange(quantity - 1)
-        newshow.artist_id = random.randrange(quantity - 1)
+    for x in range(int(quantity / 2)):
+        newshow = Show()
+        venue_id_list = [i[0] for i in db.session.query(Venue.id).all()]
+        artist_id_list = [i[0] for i in db.session.query(Artist.id).all()]
+
+        newshow.venue_id = random.choice(venue_id_list)
+        newshow.artist_id = random.choice(artist_id_list)
         newshow.start_time = datetime.now() + timedelta(days=random.randrange(3))
+
         db.session.add(newshow)
         db.session.commit()
