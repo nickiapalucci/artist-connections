bcaa72e617a2c5c23244873cddd28e8189ee4be8
nickiapalucci <cukid9876@outlook.com>
2021-07-28 02:46:08 +0000

    move model definitions into a new seperate file

--- app.py
+++ app.py
@@ -5,9 +5,8 @@
 import json
 import dateutil.parser
 import babel
-from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from flask import render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
@@ -15,63 +14,16 @@ from flask_wtf.csrf import CSRFProtect
 from forms import *
 from flask_migrate import Migrate
 from datetime import datetime
+from models import app, db, Venue, Artist, Show
+
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
 
-app = Flask(__name__)
 moment = Moment(app)
-app.config.from_object('config')
-db = SQLAlchemy(app)
 csrf = CSRFProtect(app)
 csrf.init_app(app)
-
 migrate = Migrate(app, db)
-
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-class Venue(db.Model):
-  __tablename__ = 'Venue'
-
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String)
-  city = db.Column(db.String(120))
-  state = db.Column(db.String(120))
-  address = db.Column(db.String(120))
-  phone = db.Column(db.String(120))
-  image_link = db.Column(db.String(500))
-  facebook_link = db.Column(db.String(120))
-  seeking_talent = db.Column(db.Boolean())
-  seeking_description = db.Column(db.String())
-  genres = db.Column(db.ARRAY(db.String()))
-  website = db.Column(db.String(120))
-  shows = db.relationship('Show', backref='Venue', passive_deletes=True, lazy=True)
-
-class Artist(db.Model):
-  __tablename__ = 'Artist'
-
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String)
-  city = db.Column(db.String(120))
-  state = db.Column(db.String(120))
-  phone = db.Column(db.String(120))
-  genres = db.Column(db.ARRAY(db.String()))
-  image_link = db.Column(db.String(500))
-  facebook_link = db.Column(db.String(120))
-  seeking_venue = db.Column(db.Boolean())
-  seeking_description = db.Column(db.String())
-  website = db.Column(db.String(120))
-  shows = db.relationship('Show', backref='Artist', passive_deletes=True, lazy=True)
-
-class Show(db.Model):
-  __tablename__ = 'Show'
-
-  id = db.Column(db.Integer, primary_key=True)
-  venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id', ondelete="CASCADE"), nullable=False)
-  artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id', ondelete="CASCADE"), nullable=False)
-  start_time = db.Column(db.DateTime())
 
 #----------------------------------------------------------------------------#
 # Filters.
